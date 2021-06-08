e2728e11085401bcc36ae3ab66814461608fbadd
alexacost <79877564+alexacost@users.noreply.github.com>
2021-06-08 00:05:00 -0300

    Delete checkall.js

--- consola/js/checkall.js
+++
@@ -1,6 +0,0 @@
-function toggle(source) {
-    checkboxes = document.getElementsByName('foo');
-    for(var i=0, n=checkboxes.length;i<n;i++) {
-      checkboxes[i].checked = source.checked;
-    }
-  }
